3563b0f1fa2708e07db831c352079d8ea0fb7f10
Sean Harrington <miles9080@gmail.com>
2022-03-23 02:59:10 -0700

    did not like previous code
    got rid of repeat code

--- Units.py
+++ Units.py
@@ -1,39 +1,50 @@
 # This file defines and represents what a unit is
 
-# Unit stats
-def units():
-    health = 0
-    abilitys = 0
-    sanity = 0
-
-
-# shows what a unit can do in combat
-def unit_turn():
-    attack1 = 0
-    attack2 = 0
-    attack3 = 0
-    attack4 = 0
-
-
 class Unit:
     """ The Definition of A unit """
 
     # things that make up a unit
     player = ["Class"]
+    player.health = 20.0
+    player.sanity = 10.0
+    player.name = name
+    player.ability = [6]
 
     def __init__(self, health, sanity):
         self.health = health
         self.sanity = sanity
 
+    def __int__(self, stats):
+        self.Acc = 0
+        self.Crit = 0
+        self.DMG = 0
+        self.Dodge = 0
+        self.Prot = 0
+        self.Spd =0
 
     def command(self, turn):
-        if turn == self.name
+        if turn == self.name:
             self.attack(1)
-        elif turn == self.health
+        elif turn == self.name:
             self.attack(2)
-        elif turn == self.health
+        elif turn == self.name:
             self.attack(3)
-        elif turn == self.health
+        elif turn == self.name:
             self.attack(4)
+        elif turn == self.name: # this option represents changing places with another ally
+            self.attack(5)
         else:
-            self.name "pass turn"
+            self.name: "cancel"
+
+    def movment(self):
+        for i in range(hallway):
+            print("working")
+
+
+class Unit():
+    GraveRobber = unit()
+    GraveRobber.health = 20.0
+    GraveRobber.sanity = 10.0
+    GraveRobber.name = wilma
+    GraveRobber.ability = [6]
+
