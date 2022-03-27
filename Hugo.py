e1973b3eb33b8d7ff2f3b0ab6de4078b8a766f72
Sean Harrington <miles9080@gmail.com>
2022-03-26 23:37:37 -0700

    changed test file

--- Hugo.py
+++ Hugo.py
@@ -1,15 +1,15 @@
-#This is Hugo the Bot
+# This is Hugo the Bot
 
 # Hugo constructor set up array of 18 units
 # loads the data - hard code or read from file
-# creates objects and adds data - pass to constroctor
+# creates objects and adds data - pass to constructor
 # hugo needs to create 2 random characters to start with
 
 
-#Hugo is to read from the window take in information and interact with it
+# Hugo is to read from the window take in information and interact with it
 
-#Hugo Looks at screen/window
+# Hugo Looks at screen/window
 
-#Hugo Click
+# Hugo Click
 
-#Hugo use arrow keys
+# Hugo use arrow keys
